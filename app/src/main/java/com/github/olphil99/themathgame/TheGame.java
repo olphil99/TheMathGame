99ea7290bd101e910dc5822e2785defa713af90b
Olivia P <olphil99@gmail.com>
2018-04-23 18:28:04 -0500

    First commit

---
+++ app/src/main/java/com/github/olphil99/themathgame/TheGame.java
@@ -0,0 +1,13 @@
+package com.github.olphil99.themathgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TheGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_game);
+    }
+}
