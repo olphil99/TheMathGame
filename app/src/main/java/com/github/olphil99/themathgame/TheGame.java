8eea739c4639874d4274bc0b8c0b3b2005ca8dd9
Olivia P <olphil99@gmail.com>
2018-04-25 11:33:32 -0500

    started implementing timer

--- app/src/main/java/com/github/olphil99/themathgame/TheGame.java
+++ app/src/main/java/com/github/olphil99/themathgame/TheGame.java
@@ -1,5 +1,6 @@
 package com.github.olphil99.themathgame;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class TheGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_game);
+
+        new CountDownTimer(60000, 1000) {
+            public void onTick(long msUntilFinished) {
+                //timerTxt.setText(msUntilFinished / 1000 + " s");
+            }
+
+            public void onFinish() {
+                //display alert saying game is over
+            }
+        }.start();
     }
 }
