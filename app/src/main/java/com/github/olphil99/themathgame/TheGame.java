5944af39d8b5ac281840ee036d94ebe58fc0b895
Olivia P <olphil99@gmail.com>
2018-04-25 20:52:03 -0500

    rectangle

--- app/src/main/java/com/github/olphil99/themathgame/TheGame.java
+++ app/src/main/java/com/github/olphil99/themathgame/TheGame.java
@@ -1,11 +1,17 @@
 package com.github.olphil99.themathgame;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class TheGame extends AppCompatActivity {
@@ -25,7 +31,8 @@ public class TheGame extends AppCompatActivity {
 
             timerHandler.postDelayed(this, 500);
         }
-    };;
+    };
+    ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +40,18 @@ public class TheGame extends AppCompatActivity {
         setContentView(R.layout.activity_the_game);
 
         // add timerTxt to theGame.xml
-        //timerTxt = (TextView) findViewById(R.id.timerTxt);
+        timerTxt = (TextView) findViewById(R.id.timerTxt);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#195cc6"));
+        Bitmap bg = Bitmap.createBitmap(400, 800, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bg);
+        canvas.drawRect(70, 120, 330, 240, paint);
+        paint.setColor(Color.parseColor("#ffffff"));
+        canvas.drawRect(75, 125, 325, 235, paint);
+        RelativeLayout rel = (RelativeLayout) findViewById(R.id.thePage);
+        rel.setBackgroundDrawable(new BitmapDrawable(bg));
+
 
         // might have to make the start button start the timer?
         Button btn = (Button) findViewById(R.id.startBtn);
