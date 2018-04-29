3868715f424e05fcdcb2b206adbb56915e5449d3
Olivia P <olphil99@gmail.com>
2018-04-29 17:15:14 -0500

    game view

--- app/src/main/java/com/github/olphil99/themathgame/Multiplication.java
+++ app/src/main/java/com/github/olphil99/themathgame/Multiplication.java
@@ -22,6 +22,6 @@ public class Multiplication {
     }
 
     public String toString() {
-        return multiplicand + " x " + multiplier + " = ";
+        return multiplicand + " x " + multiplier;
     }
 }
